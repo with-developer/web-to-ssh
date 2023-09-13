e4a3cd7920cbea3a846b4938a8bd2f68e4dd57aa
with-developer <miki3079@naver.com>
2023-09-13 16:48:30 +0900

    Web 기본 틀

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
+from flask_socketio import emit
+import logging
+
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@socketio.on('send_command')
+def handle_command(json):
+    command = json['command']
+    print(command)
+    # 여기서 실제 명령어 처리 로직을 구현합니다.
+    emit('new_output', {'output': command})
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
